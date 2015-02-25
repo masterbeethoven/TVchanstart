f202de0ef7bd2487e5994383852f15ebe6958d53
Core <seorisachs@gmail.com>
2015-02-25 02:02:24 -0500

    so much nvm

---
+++ Assets/scripts/RandomObjects.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomObjects : MonoBehaviour {
+	
+	public Transform littleboxes;
+	public int numToSpawn;
+	public Vector3 position;
+	
+	void Awake()
+	{
+		Vector3 position = new Vector3(Random.Range(10.0F, 15.0F), 70, Random.Range(10.0F, 15.0F));
+	}
+	
+	void Start() 
+	{
+		int spawned = 0;
+		
+		while (spawned < numToSpawn)
+		{
+			position = new Vector3(Random.Range(10.0F, 15.0F), 70, Random.Range(10.0F, 15.0F));
+			Instantiate(littleboxes, position, Quaternion.identity);
+			spawned++;
+		}
+	}
+	
+
+	
+}
